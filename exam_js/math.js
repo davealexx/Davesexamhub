3ff0f655e48412130a82095f762811afda9788d3
David OKoh <davidokoh24hr@gmail.com>
2025-09-29 13:09:07 +0100

    Update math.js

--- exam_js/math.js
+++ exam_js/math.js
@@ -99,6 +99,7 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+const empty = document.querySelector('.empty');
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -155,6 +156,7 @@ function resetState(){
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
     nextButton.innerHTML = "Try Again";
     nextButton.style.display = "block";
+      empty.innerHTML = `<a href="../exam.html" class="btn btn-primary" style="text-decoration: none;">Back to Exam</a>`
  }
  function handleNextButton(){
     currentQuestionIndex++;
